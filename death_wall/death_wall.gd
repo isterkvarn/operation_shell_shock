4d867429ed4dd1b146e773b93a9ad2a370d07ec1
eliasjohansson <eljoh633@student.liu.se>
2023-11-18 14:55:23 +0100

    crash fix and added delat for some objects

--- death_wall/death_wall.gd
+++ death_wall/death_wall.gd
@@ -1,14 +1,14 @@
 extends Area2D
 
 
-@export var speed: float = 5.0
+@export var speed: float = 2000.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x += speed
+	position.x += speed*delta
 
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		body._die()
+		body.die()
