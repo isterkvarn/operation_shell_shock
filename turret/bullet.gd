de6aaecd94a031fe0b390c47ed2dba05a88f52a6
Dethcrvsh <fabianfabbe@hotmail.se>
2023-11-18 12:08:29 +0100

    Added checkpoints

--- turret/bullet.gd
+++ turret/bullet.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
+@onready var checkpoint_master: Node2D
+
 var speed: float
+
+func set_checkpoint_master(master: Node2D):
+	checkpoint_master = master
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -10,5 +15,5 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		body.hit_by_bullet()
+		checkpoint_master.restore()
 	queue_free()
