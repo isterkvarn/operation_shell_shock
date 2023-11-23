e9720f0728d5ec2d5c124b0a9a3a93ebe2d1f31d
Loppansson <loppan@e.email>
2023-11-23 21:12:35 +0100

    feat: add dynamic camera x-offset

--- player/camera/camera.gd
+++ player/camera/camera.gd
@@ -1,7 +1,16 @@
 extends Camera2D
 
+var _offset_sensitivity: int = 500
+var _x_offset: float = 0.0
+var _max_x_offset: float = 500
+var _min_x_offset: float = -200
+
+
 @onready var player = $".."
 
 
 func _physics_process(delta):
-	position.x = player.global_position.x
+	_x_offset += Input.get_axis("ss_left", "ss_right") * delta * _offset_sensitivity
+	_x_offset = clamp(_x_offset, _min_x_offset, _max_x_offset)
+	
+	position.x = player.global_position.x + _x_offset
