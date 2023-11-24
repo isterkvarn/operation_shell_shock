11b3c5c8bbb9c9ae92f6f1c2db52b5978bad5002
Loppansson <loppan@e.email>
2023-11-24 11:17:41 +0100

    feat: change vars to constants

--- player/camera/camera.gd
+++ player/camera/camera.gd
@@ -1,11 +1,12 @@
 extends Camera2D
 
-var _offset_speed: int = 800
+var OFFSET_SPEED: int = 800
+var MAX_X_OFFSET: float = 500
+var MIN_X_OFFSET: float = -200
+var X_LERP_WEIGHT: float = 0.25
+var Y_LERP_WEIGHT: float = 0.25
+
 var _x_offset: float = 0.0
-var _max_x_offset: float = 500
-var _min_x_offset: float = -200
-var _x_lerp_weight: float = 0.25
-var _y_lerp_weight: float = 0.25
 var _camera_markers: Array[Marker2D]
 
 @onready var player = $".."
@@ -19,10 +20,10 @@ func _ready():
 
 
 func _physics_process(delta):
-	_x_offset += Input.get_axis("ss_left", "ss_right") * delta * _offset_speed
-	_x_offset = clamp(_x_offset, _min_x_offset, _max_x_offset)
+	_x_offset += Input.get_axis("ss_left", "ss_right") * delta * OFFSET_SPEED
+	_x_offset = clamp(_x_offset, MIN_X_OFFSET, MAX_X_OFFSET)
 	
-	position.x = lerp(position.x, player.global_position.x + _x_offset, _x_lerp_weight)
+	position.x = lerp(position.x, player.global_position.x + _x_offset, X_LERP_WEIGHT)
 	
 	var new_y = _start_y
 	for i in range(len(_camera_markers)):
@@ -37,4 +38,4 @@ func _physics_process(delta):
 		elif player.position.x > _camera_markers[i].position.x:
 			new_y = _camera_markers[i].position.y - 1080 * 0.5 / zoom.y
 	
-	position.y = lerp(position.y, new_y, _y_lerp_weight)
+	position.y = lerp(position.y, new_y, Y_LERP_WEIGHT)
